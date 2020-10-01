450b263a1cf71e4a95804d0d0b72cd72ca5046f5
AJoh96 <alidajohannsen@gmail.com>
2020-10-01 18:32:15 +0200

    Week_40

---
+++ Week40/Lecture.py
@@ -0,0 +1,23 @@
+import turtle
+
+
+def draw_square(animal, size):
+
+    for _ in range(6):
+        animal.forward(size)
+        animal.left(90)
+
+
+screen = turtle.Screen()
+nick = turtle.Turtle
+
+#for _ in range(60):
+#    draw_square(nick, 100)
+#    nick.left(6)
+
+for index in range(60):
+    draw_square(nick, index * 5)
+    nick.left(6)
+
+screen.exitonclick()
+
